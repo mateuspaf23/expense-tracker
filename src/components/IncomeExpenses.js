b24db643ccbd1d7de79a6c278ca4583bca5169df
Mateus Fonseca <mateuspaf23@gmail.com>
2021-08-19 09:07:48 -0300

    feat: creates the income/expenses calculation logic

--- src/components/IncomeExpenses.js
+++ src/components/IncomeExpenses.js
@@ -1,14 +1,29 @@
+import { useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
+
 const IncomeExpenses = () => {
+  const { transactions } = useContext(GlobalContext);
+
+  const amounts = transactions.map((transaction) => transaction.amount);
+  const income = amounts
+    .filter((amount) => amount > 0)
+    .reduce((acc, amount) => (acc += amount), 0)
+    .toFixed(2);
+  const expenses = amounts
+    .filter((amount) => amount < 0)
+    .reduce((acc, amount) => (acc += amount), 0)
+    .toFixed(2);
+
   return (
     <div className="inc-exp-container">
       <div>
-        <h4>Income</h4>
-        <p className="money plus"> +$0.00 </p>
+        <h4> Income </h4>
+        <p className="money plus"> {income} </p>
       </div>
 
       <div>
-        <h4>Expenses</h4>
-        <p className="money minus"> +$0.00 </p>
+        <h4> Expenses </h4>
+        <p className="money minus"> {expenses} </p>
       </div>
     </div>
   );
